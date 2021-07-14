6c7e35c8791834b4e1a7094387abe8a40a829fed
Keigo Habara <k.habara.aa@gmail.com>
2021-07-15 04:02:21 +0900

    Fix dfs

--- atcoder/centroid.hpp
+++ atcoder/centroid.hpp
@@ -8,18 +8,18 @@ using namespace std;
 vector<int> centroid(const vector<vector<int>>& g) {
     const int n = int(g.size());
     vector<int> sz(n,1), ret;
-    function<int(int,int)> dfs = [&](int u,int p) {
+    auto dfs = [&](auto self, int u, int p) -> int {
         bool isCent = true;
         for (int v : g[u]) {
             if (v == p) continue;
-            sz[u] += dfs(v, u);
+            sz[u] += self(self, v, u);
             if (sz[v] > n / 2) isCent = false;
         }
         if (n - sz[u] > n / 2) isCent = false;
         if (isCent) ret.emplace_back(u);
         return sz[u];
     };
-    dfs(0, -1);
+    dfs(dfs, 0, -1);
     return ret;
 }
 
