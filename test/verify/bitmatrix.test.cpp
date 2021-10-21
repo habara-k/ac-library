885b8b6bdd80bfb2b9ab96a41a1e253bd01d4282
keigo habara <34413567+habara-k@users.noreply.github.com>
2021-10-21 15:48:50 +0900

    Add BitMatrix (#20)
    * Add BitMatrix
    
    * [auto-verifier] verify commit abc9282bf985202ccfcce1b8d492d0850b24b928
    
    Co-authored-by: GitHub <noreply@github.com>

---
+++ test/verify/bitmatrix.test.cpp
@@ -0,0 +1,34 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1308"
+
+#include <atcoder/bitmatrix>
+#include <vector>
+#include <iostream>
+
+using namespace atcoder;
+using namespace std;
+
+int main() {
+    while (true) {
+        int m, n, d; cin >> m >> n >> d;
+        if (m == 0) break;
+
+        BitMatrix<625,625> A(n*m, n*m);
+        for (int i1 = 0; i1 < n; i1++) {
+            for (int j1 = 0; j1 < m; j1++) {
+                A[i1*m + j1][i1*m + j1] = 1;
+                for (int i2 = 0; i2 < n; i2++) {
+                    for (int j2 = 0; j2 < m; j2++) {
+                        if (abs(i1 - i2) + abs(j1 - j2) == d) {
+                            A[i1*m + j1][i2*m + j2] = 1;
+                        }
+                    }
+                }
+            }
+        }
+        vector<int> b(n*m), res;
+        for (int i = 0; i < n*m; i++) cin>> b[i];
+
+        int rank = linear_equation(A, b, res);
+        cout << (rank != -1) << endl;
+    }
+}
